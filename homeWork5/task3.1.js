9fad7827842cb15423907f9db2ab182a2b0bbecf
Olha Voloshyna <goncharovaolha@gmail.com>
2025-06-16 16:31:34 +0300

    Homework5

---
+++ homeWork5/task3.1.js
@@ -0,0 +1,8 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+const number = 6;
+
+for (let index = 0; index <= 9; index++){
+    let result = numbers[index]*number
+    console.log(`${number} * ${index+1} = ${result}`)
+}
+
