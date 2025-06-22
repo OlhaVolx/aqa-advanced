5d1dabcf5e66727dbd99c9e8e79fae3c35549335
Olha Voloshyna <goncharovaolha@gmail.com>
2025-06-22 22:03:07 +0300

    Homework7

---
+++ homeWork7/task3.js
@@ -0,0 +1,57 @@
+function divide (numerator, denominator){
+    if (typeof numerator !== "number" || typeof denominator !== "number") {
+        throw new Error ("Аргумент не є числом");
+  }
+   if ( denominator ===0){
+        throw new Error ("Ділити на нуль не можна")
+    }
+    return numerator/denominator;
+}
+
+try {
+    console.log(divide(3,0));
+} catch (error){
+  console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
+
+try {
+    console.log(divide("test",25));
+} catch (error){
+   console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
+
+try {
+    console.log(divide(5,"test"));
+} catch (error){
+   console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
+
+try {
+    console.log(divide(null,10));
+} catch (error){
+   console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
+
+try {
+    console.log(divide(5,true));
+} catch (error){
+   console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
+
+try {
+    console.log(divide(2,10));
+} catch (error){
+   console.log(error.message);
+} finally {
+    console.log("Робота завершена")
+}
